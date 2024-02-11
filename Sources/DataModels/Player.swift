6a987211f35623208a91edc275d23102555eb5b5
hartti <hartti@gmail.com>
2024-02-10 16:59:04 -0800

    Changed number of the player to be non-optional

--- Sources/DataModels/Player.swift
+++ Sources/DataModels/Player.swift
@@ -11,14 +11,14 @@ import Vapor
 public struct Player: Identifiable, Hashable, Content {
   var _id: String
   public var name: String
-  public var number: String?
+  public var number: String
   public var team: String
   
   public var id: String {
     _id
   }
   
-  public init(_id: String = UUID().uuidString, name: String, number: String? = nil, team: String) {
+  public init(_id: String = UUID().uuidString, name: String = "", number: String = "", team: String) {
     self._id = _id
     self.name = name
     self.number = number
