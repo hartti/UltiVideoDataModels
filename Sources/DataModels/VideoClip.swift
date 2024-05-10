6a8e2835c2f1b6882c8760966e5c8751fa209886
hartti <hartti@gmail.com>
2024-05-10 12:38:07 +0200

    small changes to VideoCLip: gameId -> streamUrl, initilaizer now also includes events-array

--- Sources/DataModels/VideoClip.swift
+++ Sources/DataModels/VideoClip.swift
@@ -9,7 +9,7 @@ import Foundation
 import Vapor
 
 public struct VideoClip: Hashable, Content {
-  public var game: String
+  public var streamUrl: String
   public var gameTitle: String?
   public var description: String
   public var startT: Double
@@ -18,11 +18,12 @@ public struct VideoClip: Hashable, Content {
   public var tags: [Tag]?
   public var sketches: [Sketch]?
   
-  public init(game: String, gameTitle: String? = nil, description: String, startT: Double, endT: Double) {
-    self.game = game
+  public init(streamUrl: String, gameTitle: String? = nil, description: String, startT: Double, endT: Double, events: [Event]? = nil) {
+    self.streamUrl = streamUrl
     self.gameTitle = gameTitle
     self.description = description
     self.startT = startT
     self.endT = endT
+    self.events = events
   }
 }
