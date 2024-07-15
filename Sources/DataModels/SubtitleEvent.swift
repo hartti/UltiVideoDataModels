80f25e7c62dcfdd0fb07674b7d9875cfded955db
hartti <hartti@gmail.com>
2024-07-15 16:13:04 -0700

    Added initilaizer for SubtitleEvent

--- Sources/DataModels/SubtitleEvent.swift
+++ Sources/DataModels/SubtitleEvent.swift
@@ -11,4 +11,9 @@ import Vapor
 public struct SubtitleEvent: Hashable, Content {
   public var t: Double            // Timestanmp of the event in seconds
   public var text: String         // Subtitle to be displayed
+  
+  public init(t: Double, text: String) {
+    self.t = t
+    self.text = text
+  }
 }
